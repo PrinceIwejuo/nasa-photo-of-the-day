82854ac70870373f8dd8757e10c4118172f6ffb9
Prince Iwejuo <princeiwejuo@gmail.com>
2020-12-11 15:16:37 -0500

    nasaProject

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+
 
 function App() {
+  const [nasaData, setNasaData] = useState({});
+
+  useEffect(() => {
+    const fetchImg = () => {
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=wb1vnyVTQl1zl1XSLfL76SHCumpFjvf9vwibgjMT')
+      .then(res => {
+        console.log(res.data);
+        setNasaData(res.data);
+      })
+      .catch(err => {
+
+      });
+    };
+    fetchImg();
+  }, []);
   return (
     <div className="App">
+      <h1>{nasaData.title}</h1>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        Life on Mars <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <img src={nasaData.hdurl} />    
     </div>
   );
 }
